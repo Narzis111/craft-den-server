e97dfa4360f80be8b66756c58b3ff1ddd7e2d7e0
Narzis111 <narzisnahar111@gmail.com>
2024-04-26 22:16:46 +0100

    database created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -16,156 +16,94 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const itemCollection = client.db('craftDB').collection('item');
+        const userCollection = client.db('coffeeDB').collection('user');
+          
+        // to find all (read)
+        app.get('/item', async (req, res) => {
+            const cursor = itemCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+          // update korbo specific 1ta id ke tai find single params lagbe new lagbe
+        // to find single (read)
+        app.get('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await itemCollection.findOne(query);
+            res.send(result);
+        })
+        // to find single user (read)
+        app.get("/myItems/:email", async (req, res) => {
+            console.log(req.params.email);
+            const result = await itemCollection.find({ email: req.params.email }).toArray();
+            res.send(result)
+          })
+        
+        //   create data
+        app.post('/item', async (req, res) => {
+            const newItem = req.body;
+            console.log(newItem);
+            const result = await itemCollection.insertOne(newItem);
+            res.send(result);
+        })
+        // update specific id 
 
+        app.put('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updateItem = req.body;
 
+            const item = {
+          
+                $set: {
+                    image: updateItem.image,
+                    item_name: updateItem.item_name,
+                    subcategory: updateItem.subcategory,
+                    shortDescription: updateItem.shortDescription,
+                    price: updateItem.price,
+                    rating: updateItem.rating,
+                    customization: updateItem.customization,
+                    processing_time: updateItem.processing_time,
+                    stockStatus: updateItem.stockStatus,
+                    
+                }
+            }
 
-
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i8ufgdv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-// console.log(uri);
-
-
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
-
-// async function run() {
-//     try {
-//         // Connect the client to the server	(optional starting in v4.7)
-
-
-//         const coffeeCollection = client.db('coffeeDB').collection('coffee');
-//         const userCollection = client.db('coffeeDB').collection('user');
-//         // to find all (read)
-//         app.get('/coffee', async (req, res) => {
-//             const cursor = coffeeCollection.find();
-//             const result = await cursor.toArray();
-//             res.send(result);
-//         })
-
-//         // update korbo specific 1ta id ke tai find single params lagbe new lagbe
-//         // to find single (read)
-//         app.get('/coffee/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: new ObjectId(id) }
-//             const result = await coffeeCollection.findOne(query);
-//             res.send(result);
-//         })
-//         // create data
-//         app.post('/coffee', async (req, res) => {
-//             const newCoffee = req.body;
-//             console.log(newCoffee);
-//             const result = await coffeeCollection.insertOne(newCoffee);
-//             res.send(result);
-//         })
-//         // update specific id 
-
-//         app.put('/coffee/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const filter = { _id: new ObjectId(id) }
-//             const options = { upsert: true };
-//             const updatedCoffee = req.body;
-
-//             const coffee = {
-//                 $set: {
-//                     name: updatedCoffee.name,
-//                     quantity: updatedCoffee.quantity,
-//                     supplier: updatedCoffee.supplier,
-//                     taste: updatedCoffee.taste,
-//                     category: updatedCoffee.category,
-//                     details: updatedCoffee.details,
-//                     photo: updatedCoffee.photo
-//                 }
-//             }
-
-//             const result = await coffeeCollection.updateOne(filter, coffee, options);
-//             res.send(result);
-//         })
-
-
+            const result = await itemCollection.updateOne(filter, item, options);
+            res.send(result);
+        })
 
         // delete korbo specific 1ta id ke tai find single params lagbe new lagbe
 
-//         app.delete('/coffee/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: new ObjectId(id) }
-//             const result = await coffeeCollection.deleteOne(query);
-//             res.send(result);
-//         })
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        })
 
-
-//         //  user api
-//         // to find all user (read)
-//         app.get('/user', async (req, res) => {
-//             const cursor = userCollection.find();
-//             const result = await cursor.toArray();
-//             res.send(result);
-//         })
-
-
-//         app.post('/user', async (req, res) => {
-//             const user = req.body;
-//             console.log(user);
-//             const result = await userCollection.insertOne(user);
-//             res.send(result);
-//         })
-
-//         app.patch('/user', async (req, res) => {
-//             const user = req.body;
-//             const filter = { email: user.email }
-//             const updateDoc = {
-//                 $set: {
-//                     lastLoggedAt: user.lastLoggedAt
-//                 }
-//             }
-//             const result = await userCollection.updateOne(filter, updateDoc);
-//             res.send(result);
-//         })
-
-
-//         app.delete('/user/:id', async (req, res) => {
-//             const id = req.params.id;
-//             const query = { _id: new ObjectId(id) }
-//             const result = await userCollection.deleteOne(query);
-//             res.send(result);
-//         })
-
-
-//         // Send a ping to confirm a successful connection
-//         await client.db("admin").command({ ping: 1 });
-//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         // await client.close();
-//     }
-// }
-// run().catch(console.dir);
-
-
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Assignment-10 server is running')
